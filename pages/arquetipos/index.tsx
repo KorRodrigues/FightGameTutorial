427c9d47bf8e5e2f9679e0bdd350e91d97d33e94
Tiago Kor Rodrigues <tiagotk21@hotmail.com>
2024-07-16 23:41:18 -0300

    feat: add arquetipos and fases

--- pages/arquetipos/index.tsx
+++ pages/arquetipos/index.tsx
@@ -1,16 +1,91 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Image } from "@nextui-org/image";
 
-import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/blogpost";
-import { Projetil } from "@/components/PopoverDescription";
 
 export default function Basic() {
   return (
     <DefaultLayout title="Personagens e Arquetipos">
       <section>
         <p>
-          Personagens e Arquetipos.
+          Cada personagem possui um conjunto único de golpes e habilidades que
+          definem seu estilo de jogo e estratégia. Esses conjuntos são
+          classificados em diferentes arquétipos. Existem 4 categorias de
+          arquetipos principais:
+        </p>
+      </section>
+      <section>
+        <h2>Zoner</h2>
+        <p>
+          Os zoners são mestres em manter a distância. Com várias ferramentas de
+          zoning, que incluem ataques e projéteis de diferentes ângulos e
+          velocidades, eles controlam o espaço e impedem o avanço do oponente.
+          Este tipo de personagem é perfeito para jogadores que preferem um
+          estilo de jogo mais defensivo, focado em frustrar as tentativas de
+          aproximação do adversário e forçando erros que podem ser capitalizados
+          à distância.
+        </p>
+        <p>
+          Geralmente esse personagem não vai ter opções boas de curta distância,
+          agarrões e avanço. Justamente por isso um zoner sempre vai buscar uma
+          distância segura, empurrando e impedindo o avanço do oponente, jogando
+          mais lento e capitalizando erros do oponente a distancia.
+        </p>
+      </section>
+      <section>
+        <h2>Rushdown</h2>
+        <p>
+          Os personagens de rushdown são agressivos e especializados em
+          pressionar o oponente incessantemente. Eles se destacam em abrir
+          defesas e causar danos rápidos e consistentes. Combinam golpes
+          rápidos, mix-ups e pressão contínua para dominar a luta. Embora sejam
+          rápidos e eficientes em curtas distâncias, geralmente têm poucas
+          opções defensivas e dependem da proximidade para serem eficazes.
+        </p>
+        <p>
+          Os personagens de rushdown são a escolha ideal para jogadores que
+          adoram estar na ofensiva constantemente. Eles se destacam em utilizar
+          ataques que atingem diferentes hitboxes, com lows e overheads na
+          tentativa de encontrar uma brecha na defesa do oponente e causar danos
+          rápidos e consistentes. Este arquétipo combina golpes rápidos, mix-ups
+          e uma pressão contínua que pode facilmente dominar o adversário,
+          tornando cada luta uma dança frenética e implacável.
+        </p>
+      </section>
+      <section>
+        <h2>Grappler</h2>
+        <p>
+          Um grappler se caracteriza por arremessos e agarrões de alto dano.
+          Esses personagens tendem a ter ataques poderosos que ignoram a defesa
+          do oponente, tem mais vida que a média de outros personagens, golpes
+          com armadura e opções de defesa, mas geralmente são mais lentos, não
+          costumam ter opções de longo alcance e são vulneráveis a zoning por
+          precisar se aproximar para serem eficazes.
+        </p>
+        <p>
+          Os grapplers são personagens de alto risco e alta recompensa, exigindo
+          paciência e habilidade para se aproximar e causar muito dano. É comum
+          ver grapplers virarem a partida pelo alto dano, sendo uma ótima
+          escolha para jogadores que gostam de um estilo de jogo pacientes e
+          diretos.
+        </p>
+      </section>
+      <section>
+        <h2>Shoto</h2>
+        <p>
+          Os shotos são os &quot;pau pra toda obra&quot; dos jogos de luta,
+          oferecendo um kit balanceado com diversas ferramentas e opções. Os
+          protagonistas dos jogos costumam pertencer a esse arquetipo. Eles
+          possuem ataques de distância, golpes de avanço e boas alternativas
+          defensivas, tornando-os versáteis em qualquer situação. Este arquétipo
+          é ideal para jogadores que desejam flexibilidade e a capacidade de se
+          adaptar rapidamente a diferentes adversários e estilos de jogo.
+        </p>
+        <p>
+          Embora raramente sejam os melhores em uma única área específica, os
+          shotos brilham por sua capacidade de lidar com diversas situações em
+          uma partida. Eles são especialmente apropriados para iniciantes, pois
+          ensinam fundamentos e estratégias variadas, além de serem
+          relativamente fáceis de executar.
         </p>
       </section>
     </DefaultLayout>
--- pages/fases-round/index.jsx
+++ pages/fases-round/index.jsx
@@ -6,7 +6,190 @@ export default function Basic() {
   return (
     <DefaultLayout title="Fases de um round">
       <section>
-        <p>Fases de um round.</p>
+        <p>
+          Em um round de jogo de luta, a ação se desenrola em várias fases
+          distintas, cada uma com suas próprias estratégias e dinâmicas.
+        </p>
+        <p>
+          Entender essas fases não só enriquece a experiência de assistir a
+          jogos de luta, mas também permite uma apreciação mais profunda da
+          complexidade e habilidade envolvidas em cada round. Cada fase traz
+          seus próprios desafios e estratégias.
+        </p>
+      </section>
+      <section>
+        <h2>Inicio/Opening</h2>
+        <p>
+          O início de um round muda de jogo para jogo e essas mudanças tem
+          impactos mais profundos do que muitos jogadores consideram.
+        </p>
+        <p>
+          Em Guilty Gear os personagens começam muito próximos, permitindo que
+          logo ao fim da contagem de inicio já seja possível dar um golpe, tendo
+          uma dinâmica meio jokenpo, dependendo dos golpes selecionados a fase
+          neutra pode ser totalmente pulada indo imediatamente para fase de
+          pressão ou fase de knockout.
+        </p>
+        <p>
+          Em Street fighter a distância é maior o que também aumenta a duração
+          da fase neutra, personagens zoners tem mais tempo para se preparar e
+          decidir seus golpes
+        </p>
+        <p>
+          Outros jogos podem permitir que os jogadores se movam livremente antes
+          do round iniciar ou tendo outras dinâmicas que alteram as estratégias
+          no início do round.
+        </p>
+      </section>
+      <section>
+        <h2>Neutro</h2>
+        <p>
+          Quando os personagens estão em pé de igualdade sem nenhuma pressão de
+          um sobre o outro nós chamamos de jogo neutro. Nesse momento os
+          jogadores estão decidindo as melhores opções para iniciar uma agressão
+          e/ou dominar espaço. Movimentação, uso de projéteis e ataques rápidos
+          são comuns enquanto cada um tenta ganhar terreno e forçar o oponente a
+          cometer um erro.
+        </p>
+        <p>
+          No neutro, o conhecimento das distâncias e tempo de recuperação dos
+          golpes é crucial. Entender quando e onde atacar pode definir o curso
+          do round. Os jogadores que conseguem dominar essa fase geralmente são
+          aqueles que entendem bem as mecânicas do jogo e a natureza do
+          adversário, podendo assim preparar o terreno para começar a pressão em
+          cima do oponente ao encontrar uma brecha.
+        </p>
+      </section>
+      <section>
+        <h2>Pressão/Defesa</h2>
+        <p>
+          Quando um jogador consegue forçar seu oponente a uma posição
+          defensiva, a fase de pressão começa. Neste momento, o agressor utiliza
+          uma série de ataques rápidos e mix-ups para tentar abrir a defesa do
+          oponente e causar dano. Mix-ups são combinações de golpes que variam
+          entre altos, baixos e arremesss, tornando difícil para o oponente
+          adivinhar e defender todos os ataques corretamente.
+        </p>
+        <p>
+          Por outro lado, o jogador que está na defensiva precisa ter reflexos
+          rápidos e um bom entendimento das possíveis opções do adversário para
+          bloquear adequadamente e procurar uma brecha para contra-atacar. Uma
+          defesa sólida pode virar o jogo, fazendo com que o turno vire e a
+          pressão se inicie no jogador que estava atacando anteriormente.
+        </p>
+        <p>
+          Conceitos usados na pressão:
+          <ul>
+            <li>
+              <b>Mix-ups:</b> Usar uma variedade de ataques altos, baixos, que
+              mudam a posição e arremessos para confundir a defesa do oponente.
+              Mix-ups eficazes forçam o oponente a adivinhar a direção correta
+              para bloquear ou evitar os ataques.
+            </li>
+            <li>
+              <b>Hit Confirm:</b> Ao executar um ataque, ver que o ataque
+              acertou com sucesso e então reagir a essa informação continuando o
+              combo. Ou seja, você “confirma” que seu primeiro ataque acertou
+              antes de lançar novos ataques e, se o ataque foi bloqueado, você
+              para e não prossegue com mais nada. Isso é importante porque,
+              normalmente, você cancelará um movimento que seria inseguro se
+              fosse bloqueado.
+            </li>
+            <li>
+              <b>Frame-trap:</b> Ataques que deixam pequenas aberturas que, se o
+              oponente tentar contra-atacar, resultam em punição. Frame traps
+              exploram o tempo de recuperação dos golpes para pegar o oponente
+              tentando apertar botões.
+            </li>
+            <li>
+              <b>Corner Pressure:</b> Quando o oponente está encurralado no
+              canto, sua capacidade de escapar é limitada. Utilizar o canto para
+              aumentar a pressão e aplicar mix-ups é uma estratégia chave na
+              fase de pressão. Certos combos são devastadores então o jogador
+              defensivo vai focar em defender golpes que iniciam combos.
+            </li>
+            <li>
+              <b>Safe Jumps:</b> Saltos que, mesmo se o oponente usar um golpe
+              de invencibilidade, o atacante consegue bloquear ou continuar a
+              pressão.
+            </li>
+            <li>
+              <b>Meaties:</b> Atacar o oponente no momento exato em que ele se
+              levanta do chão, forçando-o a bloquear ou ser atingido.
+            </li>
+            <li>
+              <b>Mind Games e condicionamento:</b> Fazer o oponente acreditar
+              que você vai usar uma determinada sequência de ataques e então
+              mudar para algo diferente. Isso cria incerteza e aumenta a
+              eficácia dos mix-ups e frame traps.
+            </li>
+          </ul>
+        </p>
+      </section>
+      <section>
+        <h2>Derrubado</h2>
+        <p>
+          Alguns golpes podem lançar o oponente ao chão como rasteiras,
+          arremessos e alguns espciais. Na maioria dos jogos, enquanto você está
+          deitado você é invencível a todos os ataques. O momento em que você se
+          levanta e fica vulnerável novamente, chamado de wake up ou okizeme. Se
+          você estiver no ataque, você pode optar por atacar com o hit
+          acontecendo exatamente no instante que o oponente se levanta ou
+          executar algum mixup para confundir o opoente.
+        </p>
+        <p>
+          Se você estiver se recuperando do knockdown, normalmente deverá tentar
+          bloquear, mas também pode optar por um golpe reversal para escapar ou
+          tentar abrir. Knockdowns podem levar a necessidade de apostar
+          corretamente as opções de defesa para maximizar sua vantagem nessas
+          situações e jogar bem os jogos mentais.
+        </p>
+      </section>
+      <section>
+        <h2>Fase de combos</h2>
+        <p>
+          Após acertar um golpe na fase neutra ou pressão, se inicia
+          temporariamente a fase de combo.
+        </p>
+        <p>
+          No momento que o jogador atacante percebe que um golpe acertou o
+          oponente, ele precisa analisar algumas variaveis na batalha para tomar
+          os proximos passos. O jogador vai analisar qual golpe acertou, se foi
+          um contra ataque, se atingiu o inimigo no ar e quais recursos estão
+          disponíveis, com tudo isso em mente ele vai considerar as opções de
+          combo que possui e ecolher a mais apropriada para o momento.
+        </p>
+        <p>
+          Nem sempre o combo que causa mais dano é o escolhido, em diversas
+          situações combos que jogam o oponente para o corner ou que permitem
+          manter a pressão são mais apropriados, ocasionalmente não gastar
+          recursos como barra de especial também são alternativas consideradas
+          pelo jogador.
+        </p>
+        <p>
+          Em alguns jogos o jogador defensor tem recursos para encerrar
+          prematuramente a fase de combos, como a barra de burst de guilty gear
+          strive, ou o combo breaker em killer instinct, já em outros jogos o
+          defensor apenas assiste torcendo para um erro de execução do jogador
+          atacante.
+        </p>
+      </section>
+      <section>
+        <h2>Fase de Clutch</h2>
+        <p>
+          O momento de clutch é talvez o mais emocionante de um round de jogo de
+          luta. Isso acontece quando ambos os jogadores estão com pouca vida e
+          qualquer erro pode ser fatal. A tensão está no auge, e cada movimento
+          deve ser calculado com precisão. Este é o momento onde a habilidade, a
+          mente estratégica e a calma sob pressão se destacam.
+        </p>
+        <p>
+          Durante o clutch, jogadores experientes usam mind games para forçar o
+          oponente a cometer um erro, seja através de falsos movimentos, fintas
+          ou antecipando as ações do adversário. A vitória ou derrota nesta fase
+          é muitas vezes decidida pela habilidade de manter a compostura e fazer
+          a leitura correta do adversário em um cenário de alta pressão.
+        </p>
       </section>
     </DefaultLayout>
   );
